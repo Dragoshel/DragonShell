5edaa35cf3ab9a044ff2465f81a9596fc3f198e5
Dragon <ionescu.dragos23@gmail.com>
2020-02-25 11:07:25 +0100

    final update

--- dsh.c
+++ dsh.c
@@ -10,8 +10,65 @@
 
 char **dsh_split_line(char *line);
 char *dsh_read_line();
+
 int dsh_execute(char **args);
 int dsh_launch(char **args);
+int dsh_cd(char **args);
+int dsh_help(char **args);
+int dsh_exit(char **args);
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"};
+
+int (*builtin_func[])(char **) = {
+    &dsh_cd,
+    &dsh_help,
+    &dsh_exit};
+
+int dsh_num_builtins()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int dsh_exit(char **args)
+{
+    return 0;
+}
+
+int dsh_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "dsh: expected argument to \"cd\"\n");
+    }
+    else
+    {
+        if (chdir(args[1]) != 0)
+        {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int dsh_help(char **args)
+{
+    printf("Dragos DSH\n");
+    printf("Type command name and arguments, and hit enter.\n");
+    printf("The following commands are build in: \n");
+
+    printf("%s", builtin_str[0]);
+
+    for (int i = 1, length = dsh_num_builtins(); i < length; i++)
+    {
+        printf(", %s", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
 
 int dsh_launch(char **args)
 {
@@ -19,11 +76,11 @@ int dsh_launch(char **args)
     int status;
 
     pid = fork();
-    if (pid < 0)
+    if (pid < 0) //error
     {
         perror("dsh");
     }
-    else if (pid == 0)
+    else if (pid == 0) //child
     {
         if (execvp(args[0], args) == -1)
         {
@@ -31,7 +88,7 @@ int dsh_launch(char **args)
         }
         exit(EXIT_FAILURE);
     }
-    else
+    else //parent
     {
         do
         {
@@ -127,10 +184,29 @@ void dsh_loop()
         printf("> ");
         line = dsh_read_line();
         args = dsh_split_line(line);
+        status = dsh_execute(args);
 
         free(line);
         free(args);
-    } while (1);
+    } while (status);
+}
+int dsh_execute(char **args)
+{
+    if (args[0] == NULL)
+    {
+        // An empty command was entered.
+        return 1;
+    }
+
+    for (int i = 0, length = dsh_num_builtins(); i < length; i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return dsh_launch(args);
 }
 
 int main(int argc, char **args)
